c17b25b8909b977769c489731c13d352ff60ae51
evandrotvc <evandro-tvc@hotmail.com>
2023-03-29 16:04:51 -0300

    fix: linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-    validates :name,  :email, :birth_date, presence: true
-    validates :name, length: { minimum: 3 }
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-
+  validates :name, :email, :birth_date, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
